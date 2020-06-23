c71b88201ea0155ed1dcc324b4921fa259f8764b
TakahashiTaisei <takahashi@cvlab.cs.tsukuba.ac.jp>
2020-06-23 21:57:18 +0900

    first commit

--- FilenameToTxtfile.py
+++ FilenameToTxtfile.py
@@ -1,1 +1,14 @@
-iiii
+import os
+
+path = "/home/takahashi/dataset/ColumbiaUncompressedImageSplicingDetection/4cam_splc"
+write_path = "./test.txt"
+
+files = os.listdir(path)
+
+f = open(write_path,mode="w")
+
+for i in files:
+    if i != "Thumbs.db" and i != ".DS_Store" and  i != "edgemask":
+        f.write(i+"\n")
+
+
